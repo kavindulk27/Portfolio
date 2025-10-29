19fb01f7f6fa624226e402f7744538f4c861d4bb
Kavindu Kumanayaka <kavindukumanayaka@gmail.com>
2025-10-29 15:15:57 +0530

    update

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist"),
+     outDir: 'dist',
     emptyOutDir: true,
   },
   publicDir: path.resolve(__dirname, "client/public"),
